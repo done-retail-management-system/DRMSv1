fc5ff26f1a446053c362cec2271b9cce0bc1e5ba
Thapelo <27338053@student.g.nwu.ac.za>
2019-08-30 16:50:41 +0200

    Logo
    Image made transparent

--- source/components/Logo.js
+++ source/components/Logo.js
@@ -20,7 +20,7 @@ const Logo = () => {
 const styles = StyleSheet.create({
   container : {
     flex: 1,
-    justifyContent:'center',
+    justifyContent:'flex-end',
     alignItems: 'center',
   },
   logoText:{
